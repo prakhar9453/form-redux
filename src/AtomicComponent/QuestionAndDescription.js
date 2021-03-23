834b151a74c2af846be86e4e3f0f69430cb9af01
Prakhar Chaturvedi <prakharchaturvedi@Prakhars-MacBook-Pro.local>
2021-03-23 16:07:51 +0530

    fix commit

--- src/AtomicComponent/QuestionAndDescription.js
+++ src/AtomicComponent/QuestionAndDescription.js
@@ -3,7 +3,7 @@ export function Question(props){
     return(
         <div id="QuestionandDescriptionContainer">
            <p  id="Question">{props.question}
-           {props.required==="1" && <b id="isRequired">*</b>}
+           {props.required==="1" && <span style={{color:"red",fontSize:"1.2vw"}}>*</span>}
            </p>
            {props.isDespcription==="1" && <p id="QuestionDescription">{props.description}</p>}
         </div>
